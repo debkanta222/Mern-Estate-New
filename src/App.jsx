ef433dfea51d05ad4585b884427177916ab3a596
Debkanta Chakraborty <debkanta.chakraborty@vyrazu.com>
2024-01-19 12:30:59 +0530

    common component moved from main.jsx to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Navbar from "./components/navbar";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Navbar from "../components/navbar";
 export default function Home() {
   return (
     <>
-      <Navbar />
       <h1 className="ta-center">Home</h1>
     </>
   );
